7658dd6ea97abff6f34b20ac7c894d77fefbfc4e
extremq <45830561+extremq@users.noreply.github.com>
2024-09-14 00:03:10 +0300

    more exercises

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,26 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut transformed: Vec<String> = Vec::new();
+
+        for element in input {
+            let mut t_element = element.0.clone();
+            match element.1 {
+                Command::Uppercase => t_element = t_element.to_uppercase(),
+                Command::Trim => t_element = t_element.trim().to_string(),
+                Command::Append(times) => {
+                    for _ in 0..times {
+                        t_element.push_str("bar");
+                    }
+                }
+            };
+
+            transformed.push(t_element);
+        }
+
+        transformed
+    }
 }
 
 fn main() {
@@ -37,7 +56,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
